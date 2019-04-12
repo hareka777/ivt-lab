45e1e154ce9e65d41691c8608f9fa555c355b2e9
hareka777 <37445999+hareka777@users.noreply.github.com>
2019-04-12 09:31:34 +0200

    Update TorpedoStore.java
    modifyA

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -11,7 +11,8 @@ public class TorpedoStore {
 
   // rate of failing to fire torpedos [0.0, 1.0]
   private double FAILURE_RATE = 0.0; //NOSONAR
-
+  
+  private double newParam = 0;
   private int torpedoCount = 0;
 
   public TorpedoStore(int numberOfTorpedos){
